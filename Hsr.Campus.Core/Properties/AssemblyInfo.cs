8f5e27767d4c875d97bb20c4bfb3731221f4e315
Sascha Gschwind <sascha.gschwind@hsr.ch>
2018-06-22 09:02:41 +0200

    Set release version

--- Hsr.Campus.Core/Properties/AssemblyInfo.cs
+++ Hsr.Campus.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ namespace Hsr.Campus.Core.Properties
     public static class AssemblyInfo
     {
         public const string AssemblyVersion = "3.0.*";
-        public const string AssemblyFileVersion = "3.0.2018.0621";
+        public const string AssemblyFileVersion = "3.0.2018.0622";
         public const string AssemblyInformationalVersion = AssemblyFileVersion + "-" + Compile;
         public const string Company = "Hochschule für Technik Rapperswil";
         public const string Copyright = "Copyright © HSR 2018";
