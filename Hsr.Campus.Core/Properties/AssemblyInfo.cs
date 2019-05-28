7c74796421d4e4161d162c7f6135713d1ebb5a12
Sascha Gschwind <sascha.gschwind@hsr.ch>
2019-05-28 16:09:39 +0200

    Raise app version

--- Hsr.Campus.Core/Properties/AssemblyInfo.cs
+++ Hsr.Campus.Core/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ namespace Hsr.Campus.Core.Properties
 {
     public static class AssemblyInfo
     {
-        public const string AssemblyVersion = "3.1.0";
-        public const string AssemblyFileVersion = "3.1.2019.0214";
+        public const string AssemblyVersion = "3.2.0";
+        public const string AssemblyFileVersion = "3.2.2019.0528";
         public const string AssemblyInformationalVersion = AssemblyFileVersion + "-" + Compile;
         public const string Company = "Hochschule für Technik Rapperswil";
         public const string Copyright = "Copyright © HSR 2019";
