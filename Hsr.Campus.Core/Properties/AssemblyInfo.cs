b62e2d08c44f26001ca6f4156ea4f9a23ee00646
Christian Kölla <christian.koella@hsr.ch>
2019-09-17 17:36:59 +0200

    Release 3.3

--- Hsr.Campus.Core/Properties/AssemblyInfo.cs
+++ Hsr.Campus.Core/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ namespace Hsr.Campus.Core.Properties
 {
     public static class AssemblyInfo
     {
-        public const string AssemblyVersion = "3.2.0";
-        public const string AssemblyFileVersion = "3.2.2019.0611";
+        public const string AssemblyVersion = "3.3.0";
+        public const string AssemblyFileVersion = "3.3.2019.0917";
         public const string AssemblyInformationalVersion = AssemblyFileVersion + "-" + Compile;
         public const string Company = "Hochschule für Technik Rapperswil";
         public const string Copyright = "Copyright © HSR 2019";
